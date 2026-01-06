e6588c2e40644dcd6efeaea2c9374ce573fa8e58
Cyclonekitten <157323498+DevCyclonekitten@users.noreply.github.com>
2026-01-06 22:38:28 +1100

    e
    e

--- Client Updater/gui.py
+++ Client Updater/gui.py
@@ -1,7 +1,8 @@
 import systems
 import customtkinter as ctk
-import tkinter
-
+from CTkMessagebox import CTkMessagebox
+import webbrowser
+import time
 
 class GUI():
     def __init__(self,system):
@@ -11,30 +12,174 @@ class GUI():
         else:
             self.system = systems.GetForcedOperatingSystem(system)
         
-        
+    
     def TryFreshInstall(self):
-        if(self.system.HasInstalled == False):
-            self.GetUserLauncherVersionType("Linux")
+        self.GetOS()
+        self.system.GetPackages()
+        try:
+            if(self.system.packages["repository"] == "None"):
+                    
+                self.PollEULA()
+                self.FreshInstall()
+                self.RemoveAllMessages()
+                self.CheckMessages()
+                
+            else:
+                self.CheckMessages()
+                self.StartLauncher()
+        except KeyError:
+            self.system.packages["repository"]="None"
+            self.system.SetPackages()
+            self.TryFreshInstall()
+    def GetOS(self):
+        self.system.SetupLauncherDirectory()
+        if(self.system.ISActiveUser()==True):
+            return
         else:
-            self.GetUserLauncherVersionType("Linux")
+            self.GetUserLauncherVersionType(systems.GetOperatingSystemName())
+            self.system.SetupLauncherDirectory()
+            self.system.SetOS()
     def GetUserLauncherVersionType(self,suggested):
         order = ["Windows","Mac","Linux"]
         order.remove(suggested)
-        rt = tkinter.Tk()
-        rt.geometry("1x1")
-        msg = tkinter.messagebox.Message(title="Install Launcher", message=f"Trihexangular Launcher hasn't been installed on this device. \n Detected operating system: {suggested}",
-                        icon="question", option_1=suggested, option_2=order[0], option_3=order[1],option_4="Exit")
-        #response = msg.get()
-        response = "hi"
-        if response==suggested:
+        i = self.Message("Select OS", "Detected operating system: "+suggested,"question",[suggested,order[0],order[1]])
+        
+        if(i==0):
             return
-        elif response==order[0]:
+        elif(i==1):
             self = GUI(order[0])
-        elif response==order[1]:
+        elif(i==2):
             self = GUI(order[1])
-
-
-        if response=="Exit":
+        else:
+            exit()
+    def ConfirmEULA(self):
+        i = self.Message("End User Licence Agreement", ("Have you read and do you agree to the END USER LICENCE AGREEMENT (EULA)."),"question",["View Eula","Agree","Disagree"])
+        if(i==0):
+            webbrowser.open_new('https://www.example.com')
+            self.ConfirmEULA()
+        if(i==1):
+            self.FinishEULA()
+        if(i==2):
+            self.DenyEULA()
+    def FinishEULA(self):
+        self.system.GetPackages()
+        self.system.packages["eula"] = "true"
+        self.system.SetPackages()
+    def DenyEULA(self):
+        self.Message("Cannot Install","Cannot install without agreeing to EULA","cancel",["Exit"])
+        exit()
+    def PollEULA(self):
+        self.system.GetPackages()
+        try:
+            r=self.system.packages["eula"]
+            if(r=="true"):
+                return
+            else:
+                self.ConfirmEULA()
+        except KeyError:
+            self.system.packages["eula"] = "false"
+            self.system.SetPackages()
+            self.PollEULA()
+        
+    
+    def Message(self,title,message,icon,options):
+        root = ctk.CTk()
+        root.geometry("1x1")
+        msg = CTkMessagebox(title=title,message=message,icon=icon,options=options)
+        response = msg.get()
+        if(response =="Exit"):
+            
+            exit()
+        for i in range(len(options)):
+            if(response==options[i]):
+                return i
+        else:
             exit()
 
-GUI(False)
+    def FreshInstall(self):
+        self.GetRepository()
+        self.system.InstallLauncher()
+    def GetRepository(self):
+        i = self.Message("Launcher Repository","Do you want to install from the main repository 'https://github.com/DevCyclonekitten/Trihexangular-Launcher'","question",["Main","Custom","Exit"])
+        docustom = i
+        customlink = "https://github.com/DevCyclonekitten/Trihexangular-Launcher"
+        if(i==1):
+            self.Message("Custom Repository", "Custom repositories are currently not supported","cancel",["Back","Exit"])
+            self.Install()
+            return
+        self.system.GetPackages()
+        self.system.packages["repository"] = customlink
+        self.system.SetPackages()
+
+    def RunMessage(self,message,ID):
+        flags = self.GetMessageFlags(message)
+
+        if(flags[3]==1):
+            return
+
+        i = self.Message(message["name"],message["content"],flags[0],flags[4])
+
+        interactor = message["interactions"][i]
+
+
+        if(message["jsonmodify"]!={}):
+            for key in message["jsonmodify"].keys():
+                self.system.packages[key]=message["jsonmodify"][key]
+
+        if(interactor["type"]=="accept"):
+            pass
+        elif(interactor["type"]=="url"):
+            webbrowser.open_new(interactor["content"])
+        elif(interactor["type"]=="messagebuilder"):
+            self.RunMessage(interactor["content"],-1)
+        
+        if(not flags[2]==1):
+            if(not ID ==-1):
+                self.system.GetPackages()
+                self.system.packages["messages"].append(ID)
+                self.system.SetPackages()
+        if(flags[1]==1):
+            time.sleep(0.1)
+            exit()
+    def GetMessageFlags(self,message):
+        # Message Flags
+        icons = ["check","cancel","info","question","warning"]
+        iconflag = icons[int(message["flags"][0])]
+        exitflag = int(message["flags"][1])
+        perminance = int(message["flags"][2])
+        #invisible = int(message["invisible"])
+        invisible = 0
+
+        buttons = message["interactions"]
+        buttonnames = []
+        for button in buttons:
+            buttonnames.append(button["name"])
+
+
+        return [iconflag,exitflag,perminance,invisible,buttonnames]
+    def CheckMessages(self):
+        self.system.GetLauncherData()
+        self.system.GetPackages()
+        for item in self.system.data["launcher"]["maintainer"]["messages"].keys():
+            viewed = False
+            for viewedItem in self.system.packages["messages"]:
+                if(item==viewedItem):
+                    viewed = True
+            if(not viewed):
+                m = self.system.data["launcher"]["maintainer"]["messages"][item]
+                self.RunMessage(m,item)
+    def RemoveAllMessages(self):
+        self.system.GetPackages()
+        for item in self.system.data["launcher"]["maintainer"]["messages"].keys():
+            viewed = False
+            for viewedItem in self.system.packages["messages"]:
+                if(item==viewedItem):
+                    viewed = True
+            if(not viewed):
+                self.system.packages["messages"].append(item)
+        self.system.SetPackages()
+    def StartLauncher(self):
+        self.system.StartLauncher()
+        
+        
+GUI(False)  
--- Client Updater/systems.py
+++ Client Updater/systems.py
@@ -2,12 +2,13 @@ import platform
 import os
 import network
 import json
+import subprocess
 
 
 
 
 urls = {
-    "launcher_data":r"https://github.com/DevCyclonekittenTriHex/trihexangulargamesrepo/raw/main/data/launcher_data.json",
+    "launcher_data":r"https://github.com/DevCyclonekittenTriHex/Trihexangular-Launcher/raw/main/Server/data/launcher_data.json",
     "launcher":r"https://github.com/DevCyclonekittenTriHex/trihexangulargamesrepo/releases/download/main_update_0.95/Launcher_2.0.zip"
 }
 def GetOperatingSystem():
@@ -25,8 +26,19 @@ def GetOperatingSystem():
         c=Java()
     
     return c
-
-
+def GetOperatingSystemName():
+    n= "Windows"
+    n = platform.system()
+    
+    c =""
+    if(n=="Linux"):
+        return "Linux"
+    elif(n=="Darwin"):
+        return "Mac"
+    elif(n=="Windows"):
+        return "Windows"
+    elif(n=="Java" or n=="FreeBSD"):
+        return "Java"
 
 
 
@@ -43,7 +55,20 @@ class Linux():
             "data": os.path.join(os.path.expanduser("~")+"/.trihexangular-launcher","data"),
             "games": os.path.join(os.path.expanduser("~")+"/.trihexangular-launcher","games"),
         }
-
+    def SetOS(self):
+        self.GetPackages()
+        self.packages["system"]="Linux"
+        self.SetPackages()
+    def ISActiveUser(self):
+        
+        self.GetPackages()
+        try:
+            if(self.packages["system"]=="Linux"):
+                return True
+            else:
+                return False
+        except Exception as e:
+            return False
     def HasInstalled(self):
         if(os.path.isdir(self.paths["folder"])):
             return True
@@ -52,40 +77,58 @@ class Linux():
     def SetupLauncherDirectory(self):
         for path in self.paths.keys():
             os.makedirs(self.paths[path],exist_ok=True)
-    
     def GetLauncherData(self):
-        #self.network.InstallFile(urls["launcher_data"],self.paths[".temp"],self.paths["data"],"launcher_data.json")
+        p = self.packages["repository"] + "/raw/main/Server/data/launcher_data.json"
+
+
+        self.network.InstallFile(p,self.paths[".temp"],self.paths["data"],"launcher_data.json")
+
         with open(os.path.join(self.paths["data"],"launcher_data.json"), "r") as f:
             self.data = json.load(f)
-    def InstallLauncher(self):
-        url = urls["launcher"]
-        self.network.InstallZip(url,os.path.join(self.paths[".temp"],"launcher_download_linux_x86_64"),self.paths["bin"])
+    def StartLauncher(self):
+        path = os.path.join(self.paths["bin"],("Trihexangular Launcher"+""))
 
+        print(path)
+        subprocess.Popen([path])
+    def InstallLauncher(self):
+        self.GetLauncherData()
+        self.GetPackages()
+        print(self.data)
+        launcher_number = self.data["launcher"]["version"]
+        self.network.InstallZip(self.packages["repository"]+f"/releases/main_{launcher_number}/launcher_linux",os.path.join(self.paths[".temp"],"launcher_download_linux_x86_64"),self.paths["bin"])
     def CheckForUpdates(self):
         
         self.GetLauncherData()
         self.GetPackages()
-        
-        if(self.packages["launcher"]["version"]<self.data["launcher"]["version"]):
-            self.InstallLauncher()
-            self.packages["launcher"]["version"]=self.data["launcher"]["version"]
+        try:
+            if(self.packages["launcher"]["version"]<self.data["launcher"]["version"]):
+                self.InstallLauncher()
+                self.packages["launcher"]["version"]=self.data["launcher"]["version"]
+                self.SetPackages()
+        except KeyError:
+            print("KeyError")
+            self.packages["launcher"]={
+                "version":-1
+            }
             self.SetPackages()
-    
-
-    
+            self.CheckForUpdates()
     def GetPackages(self):
         try:
             with open(os.path.join(self.paths["data"],"packages.json"), "r") as f:
                 self.packages = json.load(f)
-        except FileNotFoundError:
+        except Exception as e:
             self.packages = {
                 "launcher":{
                     "version":0.94
-                }
+                },
+                "system":"None",
+                "eula":"false",
+                "repository":"None",
+                "messages":[]
             }
             with open(os.path.join(self.paths["data"],"packages.json"),"w") as f:
                 json.dump(self.packages, f)
-            self.GetPackages(self)
+            self.GetPackages()
             return
     def SetPackages(self):
         with open(os.path.join(self.paths["data"],"packages.json"),"w") as f:
