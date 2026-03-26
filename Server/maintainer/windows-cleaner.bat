d686743d75ab317d566793d51c3e217092ea626b
Cyclonekitten <157323498+DevCyclonekitten@users.noreply.github.com>
2026-03-26 21:17:05 +1100

    e
    e

--- Server/maintainer/windows-cleaner.bat
+++ Server/maintainer/windows-cleaner.bat
@@ -1,4 +1,5 @@
 @echo off
+echo "Deleting paths: User\LocalLow\trihexangular-launcher, \User\.trihexangular-launcher"
 rmdir %LOCALAPPDATA%\LocalLow\trihexangular-launcher
-rmdir 
+rmdir %USERPROFILE%\.trihexangular-launcher
 pause
